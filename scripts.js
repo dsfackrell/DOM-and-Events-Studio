1015b493d1e0d8625bc3f74d1c2b99a9db645aa8
dsfackrell <dsfackrell@gmail.com>
2021-07-22 20:22:14 -0500

    finished part 4

--- scripts.js
+++ scripts.js
@@ -5,21 +5,39 @@ window.addEventListener("load",function() {
     let takeoff = document.getElementById("takeoff");
     takeoff.addEventListener("click", function (event) {
         if(confirm("Confirm that the shuttle is ready for takeoff.")){
-            takeoffConfirmed()
+            takeoffConfirmed();
         }
     });
 
     let landing = document.getElementById("landing");
+    
     landing.addEventListener("click", function (event) {
-
+        landShuttle();        
     });
 
     let missionAbort = document.getElementById("missionAbort");
-    takeoff.addEventListener("click", function(event) {
 
+    missionAbort.addEventListener("click", function (event) {
+        if(confirm("Confirm that you want to abort the mission.")) {
+            abortMission();   
+        }
     });
 
 });
+
+function landShuttle() {
+    alert("The shuttle is landing. Landing gear engaged.");
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");    
+    let shuttleHeight = document.getElementById("spaceShuttleHeight");
+
+
+    flightStatus.innerHTML = "The shuttle has landed.";
+
+    shuttleBackground.style.backgroundColor = "green";
+
+    shuttleHeight.innerHTML = 0;
+}
 
 function takeoffConfirmed() {
     let flightStatus = document.getElementById("flightStatus");
@@ -32,4 +50,17 @@ function takeoffConfirmed() {
 
     shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML) + 10000;
 }
+
+function abortMission() {
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");    
+    let shuttleHeight = document.getElementById("spaceShuttleHeight");
+
+    flightStatus.innerHTML = "mission aborted";
+
+    shuttleBackground.style.backgroundColor = "green";
+
+    shuttleHeight.innerHTML = 0;
+}
+
 
